a8afd5351a24aa4f7112b8405466a66c94ad293d
Arbaaz <58431363+arbaazs161@users.noreply.github.com>
2022-04-02 15:41:21 +0530

    Time: 3 ms (41.58%), Space: 43.8 MB (73.56%) - LeetHub

---
+++ 90-subsets-ii/90-subsets-ii.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);  //To avoid duplicate numbers
+        
+        List<List<Integer>> ans = new ArrayList<>();
+        
+        findSubsets(0, nums, new ArrayList<>(), ans);
+        
+        return ans;
+    }
+    
+    public void findSubsets(int index, int[] nums, List<Integer> ds, List<List<Integer>> ans){
+        ans.add(new ArrayList<>(ds));   // add all subsets found at each recursive call
+        for(int i = index; i < nums.length; i++){   //Loop to check all integers from index and pick the first unique integer
+            if(i!=index && nums[i] == nums[i-1]) continue;
+            ds.add(nums[i]);
+            findSubsets(i+1, nums, ds, ans);    
+            ds.remove(ds.size() - 1);   // remove the last added integer to get all subsets at a particular level
+        }
+    }
+}
