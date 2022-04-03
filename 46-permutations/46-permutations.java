43ba0f13f127d3852cf021ec42e1b4be000495bd
Arbaaz <58431363+arbaazs161@users.noreply.github.com>
2022-04-03 15:53:12 +0530

    Time: 2 ms (70.17%), Space: 44.4 MB (67.76%) - LeetHub

---
+++ 46-permutations/46-permutations.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> ds = new ArrayList<>();
+        boolean[] freq = new boolean[nums.length];
+        findPermutations(nums, ds, ans, freq);
+        return ans;
+    }
+    
+    private void findPermutations(int[] nums, List<Integer> ds, List<List<Integer>> ans, boolean[] freq){
+        if(ds.size() == nums.length){
+            ans.add(new ArrayList<>(ds));
+            return;
+        }
+        
+        for(int i = 0; i < nums.length; i++){
+            if(!freq[i]){
+                freq[i] = true;
+                ds.add(nums[i]);
+                findPermutations(nums, ds, ans, freq);
+                ds.remove(ds.size() - 1);
+                freq[i] = false;
+            }
+        }
+    }
+}
