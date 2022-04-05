9fc3863c5dd39caa14352d775fd50b7ce819ef7f
Arbaaz <58431363+arbaazs161@users.noreply.github.com>
2022-04-06 00:02:38 +0530

    Time: 1 ms (35.01%), Space: 42.1 MB (75.88%) - LeetHub

--- 75-sort-colors/75-sort-colors.java
+++ 75-sort-colors/75-sort-colors.java
@@ -1,8 +1,35 @@
 class Solution {
     public void sortColors(int[] nums) {
+        
+        //Sol 2
+        
+        int lo = 0, hi = nums.length-1, mid = 0; int temp;
+        
+        while(mid<=hi){
+            switch(nums[mid]){
+                case 0:
+                    temp = nums[lo];
+                    nums[lo] = nums[mid];
+                    nums[mid] = temp;
+                    lo++; mid++;
+                    break;
+                
+                case 1:
+                    mid++;
+                    break;
+                    
+                case 2:
+                    temp = nums[mid];
+                    nums[mid] = nums[hi];
+                    nums[hi] = temp;
+                    hi--;
+                    break;
+            }
+        }
+        
         //Sol 1
         
-        int num0=0, num1=0, num2=0;
+        /*int num0=0, num1=0, num2=0;
         
         for(int i = 0; i < nums.length; i++){
             if(nums[i] == 0){
@@ -25,6 +52,6 @@ class Solution {
         }
         for(j=num1+num0; j < num0+num1+num2; j++){
             nums[j] = 2;
-        }
+        }*/
     }
 }
