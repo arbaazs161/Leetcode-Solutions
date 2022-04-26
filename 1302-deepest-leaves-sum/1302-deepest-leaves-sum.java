439df690b7f9ea86b720a02f4cd857f3f269a04e
Arbaaz <58431363+arbaazs161@users.noreply.github.com>
2022-04-26 18:09:15 +0530

    Time: 1 ms (100.00%), Space: 44.8 MB (86.57%) - LeetHub

--- 1302-deepest-leaves-sum/1302-deepest-leaves-sum.java
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.java
@@ -14,7 +14,9 @@
  * }
  */
 class Solution {
-    public int deepestLeavesSum(TreeNode root) {
+    
+    //Approach 1 : 
+    /*public int deepestLeavesSum(TreeNode root) {
         
         //Simple : Perform Level order Traversal -> Add the values on last level of traversal and return the total
         Queue<TreeNode> q=new LinkedList<>();
@@ -31,5 +33,29 @@ class Solution {
             }
         }
         return sum;
+    }*/
+    
+    private int maxLevel = 0;
+    private int sum = 0;
+    
+    public int deepestLeavesSum(TreeNode root) {
+        if(root == null) return 0;
+        calculateSumAtLevel(root,0);
+        return sum;
+        
+    }
+    
+    private void calculateSumAtLevel(TreeNode root,int level){
+        
+       if(root == null) return;
+        if(level > maxLevel){
+            sum = 0;
+            maxLevel = level;
+        }
+        if(level == maxLevel){
+            sum = sum + root.val;
+        }
+        calculateSumAtLevel(root.left,level+1);
+        calculateSumAtLevel(root.right,level+1);
     }
 }
