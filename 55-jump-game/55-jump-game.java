c196f19b7082f42adf936eb98d41cf931d6e2094
Arbaaz <58431363+arbaazs161@users.noreply.github.com>
2022-05-13 12:17:00 +0530

    Time: 2 ms (86.31%), Space: 42.9 MB (85.07%) - LeetHub

--- 55-jump-game/55-jump-game.java
+++ 55-jump-game/55-jump-game.java
@@ -6,5 +6,28 @@ class Solution {
             reachable = Math.max(reachable, i + nums[i]);
         }
         return true;
+        
+        /*
+        
+        int len = nums.length;
+        if(len==1){
+            return true;
+        }
+        boolean[] isReachableFromZero = new boolean[len];
+        isReachableFromZero[0] = true;
+        for(int i=1;i<len;i++){
+            for(int j=i-1;j>=0;j--){
+                // If j is reachable from zero
+                // and from j, we can reach i
+                // Means we can reach i from Zero
+                if(isReachableFromZero[j]&&j+nums[j]>=i){
+                    isReachableFromZero[i]=true;
+                    break;
+                }
+            }
+        }
+        return isReachableFromZero[len-1];
+        
+        */
     }
 }
