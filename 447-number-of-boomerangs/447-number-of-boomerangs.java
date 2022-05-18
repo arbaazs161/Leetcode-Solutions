59bb77949b1e9aa92ef2b0e532393544fd32b871
Arbaaz <58431363+arbaazs161@users.noreply.github.com>
2022-05-18 15:19:04 +0530

    Time: 235 ms (45.60%), Space: 108.6 MB (72.93%) - LeetHub

---
+++ 447-number-of-boomerangs/447-number-of-boomerangs.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int numberOfBoomerangs(int[][] points) {
+        int result = 0;
+        HashMap<Integer,Integer> distMap = new HashMap<Integer,Integer>();
+        for(int[] i : points) {
+            for(int[] j : points) {
+                if(i==j) continue;
+                int dist = (i[0]-j[0])*(i[0]-j[0]) + (i[1]-j[1])*(i[1]-j[1]);
+                int prevDist = distMap.containsKey(dist) ? distMap.get(dist) : 0;
+                result += 2*prevDist;
+                distMap.put(dist, prevDist+1);
+            }
+            distMap.clear();
+        }
+        return result;
+    }
+}
