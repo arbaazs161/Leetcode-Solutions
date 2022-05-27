3bccae6ed2a31106a85a522cb97e3993951dcf80
Arbaaz <58431363+arbaazs161@users.noreply.github.com>
2022-05-27 12:25:52 +0530

    Time: 7 ms (20.63%), Space: 52 MB (10.53%) - LeetHub

---
+++ 268-missing-number/268-missing-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        Arrays.sort(nums);
+        
+        for(int i =0 ; i < nums.length; i++){
+            if(nums[i] != i){
+                return nums[i] - 1;
+            }
+        }
+        
+        return nums.length;
+    }
+}
